2841cfec1b6e4ce9178348ad276fddddb246c136
Raja Rajan Krishna Kumar <rajarajan051105@gmail.com>
2025-09-26 14:50:42 +0530

    allow all origins

--- config/config/settings.py
+++ config/config/settings.py
@@ -59,11 +59,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
 CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
